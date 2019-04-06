23d6a86caef0f3cc263e32c69d32f52b7fbd78f3
Jose Felix <jfelix000@west-mec.org>
2019-04-05 18:25:10 -0700

    Left off 13
    I left off here and Monday I will finish this for it to be complete all the way

---
+++ js_tree.js
@@ -0,0 +1,79 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5, CSS3, and JavaScript 6th Edition
+   Tutorial 12
+   Case Problem 3
+
+   Author: Jose Felix
+   Date:   4.5.19
+
+   Filename: js_tree.js
+
+   Global Variables:
+   nodeCount
+      Running count of all nodes in the source document
+   elementCount
+      Running count of all element nodes in the source document
+   textCount
+      Running count of all text nodes in the source document
+   wsCount
+      Running count of all white space text nodes in the source document
+
+
+   Functions List:
+   makeTree() 
+      Sets up and places the node tree within the HTML document and
+      displays the node counts from the document
+
+   makeBranches(treeNode, nestedList)
+      Makes a list item or an ordered list based on the contents and type
+      of node from the sourceNode parameter and then appends that list
+      item or ordered list to nestedList. The function recursively calls 
+      itself to navigate throught the node tree of the source document.
+
+   isWhiteSpaceNode(tString)
+      Returns true if tString represents the text of a white space text
+      node and false if it doesn't
+*/
+// global variables for these nodes
+var nodeCount = 0;
+var elemCount = 0;
+var textCount = 0;
+var wsCount = 0;
+
+// starts the makeTree function when the code starts
+window.onload =  makeTree;
+function makeTree() {
+      // creates an aside
+      var aside = document.createElement("aside");
+      // sets the id
+      aside.setAttribute("id", "treeBox");
+      // creates the h1
+      var h1 = document.createElement("h1");
+      // sets the text content
+      h1.textContent = "Node Tree";
+      // combines the two
+            aside.appendChild(h1);
+      document.getElementById("main").appendChild(aside);
+      var nodeList = document.createElement("ol");
+      aside.appendChild(nodeList);
+      var sourceArticle = document.querySelectorAll("#main article");
+      // calls the function using these parameters
+      makeBranches(nodeList, sourceArticle);
+}
+
+function makeBranches(treeNode, nestedList) {
+      nodeCount += 1;
+      var liElem = document.createElement("li");
+      var spanElem = document.createElement("span");
+      liElem.appendChild("+--" + spanElem);
+      liElem.appendChild(spanElem);
+      nestedList.appendChild(liElem);
+}
+
+
+// dont touch
+function isWhiteSpaceNode(tString) {
+   return !(/[^\t\n\r ]/.test(tString));
+}
